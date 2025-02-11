81287467457c2bab7a8a81dce3624c58353391c9
matouuuuuuuuuu <mathis.albrun@gmail.com>
2025-02-11 20:13:34 +0100

    ajout getters/setters/constructor

--- src/main/java/aventure/Dialogue.java
+++ src/main/java/aventure/Dialogue.java
@@ -7,4 +7,39 @@ public abstract class Dialogue extends Event {
 	private ArrayList<String> dialogues;
 	private ArrayList<String> playerResponses;
 	private int currentLine;
+	
+	public Dialogue() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getCurrentLine() {
+		return currentLine;
+	}
+	
+	public ArrayList<String> getDialogues() {
+		return dialogues;
+	}
+	
+	public String getNpcName() {
+		return npcName;
+	}
+	
+	public ArrayList<String> getPlayerResponses() {
+		return playerResponses;
+	}
+	
+	public void setCurrentLine(int currentLine) {
+		this.currentLine = currentLine;
+	}
+	
+	public void setDialogues(ArrayList<String> dialogues) {
+		this.dialogues = dialogues;
+	}
+	
+	public void setNpcName(String npcName) {
+		this.npcName = npcName;
+	}
+	
+	public void setPlayerResponses(ArrayList<String> playerResponses) {
+		this.playerResponses = playerResponses;
+	}
 }
--- src/main/java/aventure/Weapon.java
+++ src/main/java/aventure/Weapon.java
@@ -2,4 +2,20 @@ package engine.object;
 
 public abstract class Weapon extends Equipment {
 	private int damage;
+	
+	public Weapon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	
+	
+	
 }
