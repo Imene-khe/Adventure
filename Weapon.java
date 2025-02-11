bded07008656b26062fdab05a09432bac5ca9595
matouuuuuuuuuu <mathis.albrun@gmail.com>
2025-02-11 20:04:07 +0100

    ajout de getters/setters/constructors

---
+++ Weapon.java
@@ -0,0 +1,21 @@
+package engine.object;
+
+public abstract class Weapon extends Equipment {
+	private int damage;
+	
+	public Weapon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	
+	
+	
+}
