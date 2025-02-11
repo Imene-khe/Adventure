495b01615b257dc1b0bd1a30c4f4d58672eedeff
matouuuuuuuuuu <mathis.albrun@gmail.com>
2025-02-11 23:35:30 +0100

    creation du package gui et de la classe MainGUI

---
+++ src/main/java/aventure/MainGUI.java
@@ -0,0 +1,73 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import engine.map.Map;
+
+
+public class MainGUI extends JFrame implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Map map;
+
+	private final static Dimension preferredSize = new Dimension(GameConfiguration.WINDOW_WIDTH, GameConfiguration.WINDOW_HEIGHT);
+
+	private ElementInterface manager;
+
+	private GameDisplay dashboard;
+
+	public MainGUI(String title) {
+		super(title);
+		init();
+	}
+
+	private void init() {
+
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new BorderLayout());
+
+		KeyControls keyControls = new KeyControls();
+		JTextField textField = new JTextField();
+		textField.addKeyListener(keyControls);
+		contentPane.add(textField, BorderLayout.SOUTH);
+
+		map = GameBuilder.buildMap();
+		manager = GameBuilder.buildInitMobile(map);
+		dashboard = new GameDisplay(map, manager);
+
+		MouseControls mouseControls = new MouseControls();
+		dashboard.addMouseListener(mouseControls);
+
+		dashboard.setPreferredSize(preferredSize);
+		contentPane.add(dashboard, BorderLayout.CENTER);
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+		setPreferredSize(preferredSize);
+		setResizable(false);
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(GameConfiguration.GAME_SPEED);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+
+			manager.nextRound();
+			dashboard.repaint();
+		}
+	
