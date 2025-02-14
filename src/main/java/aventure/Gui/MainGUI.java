0dc2a9a068fe8a69a8f9b54c3cac67e2bd2fd568
matouuuuuuuuuu <mathis.albrun@gmail.com>
2025-02-14 15:42:00 +0100

    test de commit

--- src/main/java/aventure/Gui/MainGUI.java
+++ src/main/java/aventure/Gui/MainGUI.java
@@ -1,73 +1,57 @@
-package aventure.Gui;
+package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import engine.map.Block;
 import engine.map.Map;
 
 
 public class MainGUI extends JFrame implements Runnable {
 
-	private static final long serialVersionUID = 1L;
-
 	private Map map;
-
-	private final static Dimension preferredSize = new Dimension(GameConfiguration.WINDOW_WIDTH, GameConfiguration.WINDOW_HEIGHT);
-
-	private ElementInterface manager;
-
 	private GameDisplay dashboard;
 
 	public MainGUI(String title) {
 		super(title);
-		init();
+		initLayout();
 	}
 
-	private void init() {
-
+	private void initLayout() {
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
-
-		KeyControls keyControls = new KeyControls();
-		JTextField textField = new JTextField();
-		textField.addKeyListener(keyControls);
-		contentPane.add(textField, BorderLayout.SOUTH);
-
-		map = GameBuilder.buildMap();
-		manager = GameBuilder.buildInitMobile(map);
-		dashboard = new GameDisplay(map, manager);
-
-		MouseControls mouseControls = new MouseControls();
-		dashboard.addMouseListener(mouseControls);
-
-		dashboard.setPreferredSize(preferredSize);
-		contentPane.add(dashboard, BorderLayout.CENTER);
-
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		pack();
-		setVisible(true);
-		setPreferredSize(preferredSize);
-		setResizable(false);
+		
+		JPanel inventoryPanel = new JPanel(new GridLayout());
 	}
 
 	@Override
 	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//@Override
+	/*public void run() {
 		while (true) {
 			try {
 				Thread.sleep(GameConfiguration.GAME_SPEED);
 			} catch (InterruptedException e) {
 				System.out.println(e.getMessage());
 			}
-
-			manager.nextRound();
 			dashboard.repaint();
 		}
+	}*/
+}
+
+
 	
