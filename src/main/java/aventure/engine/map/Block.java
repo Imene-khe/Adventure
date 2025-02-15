42717bdc5e1dab2d62d949ecd10e8aaf981294f4
Imene <khelilimene03@gmail.com>
2025-02-15 21:41:28 +0100

    correction de la classe block

--- src/main/java/aventure/engine/map/Block.java
+++ src/main/java/aventure/engine/map/Block.java
@@ -1,32 +1,35 @@
 package aventure.engine.map;
 
 public class Block {
-	private int line;
-	private int column;
-	private boolean isOccuped;
+    private int line;
+    private int column;
+    private boolean isOccupied; // Correction du nom
 
-	public Block(int line, int column) {
-		this.line = line;
-		this.column = column;
-		isOccuped=false;
-	}
+    public Block(int line, int column) {
+        this.line = line;
+        this.column = column;
+        this.isOccupied = false;
+    }
 
-	public int getLine() {
-		return line;
-	}
+    public int getLine() {
+        return line;
+    }
 
-	public int getColumn() {
-		return column;
-	}
-	
-	public boolean isOccuped() {
-		return isOccuped;
-	}
+    public int getColumn() {
+        return column;
+    }
 
-	@Override
-	public String toString() {
-		return "Block [line=" + line + ", column=" + column + "]";
-	}
-	
+    public boolean isOccupied() { // Correction du getter
+        return isOccupied;
+    }
 
+    public void setOccupied(boolean isOccupied) { // Ajout du setter
+        this.isOccupied = isOccupied;
+    }
+
+    @Override
+    public String toString() {
+        return "Block [line=" + line + ", column=" + column + "]";
+    }
 }
+
